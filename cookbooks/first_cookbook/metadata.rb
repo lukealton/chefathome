85547b0c484ba38156c4d7c01be92f5fd3b2941c
Luke Alton <lucas.alton@gmail.com>
2020-05-31 13:14:45 -0600

    updated metadata for cookbook

--- cookbooks/first_cookbook/metadata.rb
+++ cookbooks/first_cookbook/metadata.rb
@@ -1,9 +1,9 @@
 name 'first_cookbook'
-maintainer 'The Authors'
+maintainer 'Lucas Alton'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures first_cookbook'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
