b29548ec59e3b3a4efd6b9ade2f440e620044566
Luke Alton <lucas.alton@gmail.com>
2020-05-31 00:13:11 -0600

    first cookbook

---
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: first_cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+
+file "#{ENV['HOME']}/test.txt" do
+  content 'Rainbow sparkle land achieved :) !'
+end
