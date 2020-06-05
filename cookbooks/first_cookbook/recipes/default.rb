1ce71b92dacfffbde78e7ad6f81e652085d97694
Luke Alton <lucas.alton@gmail.com>
2020-06-04 23:36:42 -0600

    added 7zip to default recipe

--- cookbooks/first_cookbook/recipes/default.rb
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -7,3 +7,10 @@
 file "#{ENV['HOME']}/test.txt" do
   content 'Rainbow sparkle land achieved :) !'
 end
+
+windows_package '7-Zip 19.00 (x64 edition)' do
+  checksum       'A7803233EEDB6A4B59B3024CCF9292A6FFFB94507DC998AA67C5B745D197A5DC'
+  source         'https://www.7-zip.org/a/7z1900-x64.msi'
+  installer_type :msi
+  action         :install
+end
