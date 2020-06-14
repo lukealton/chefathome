4b7648d513aea442613bee36b57ad19c44c2ae47
Luke Alton <lucas.alton@gmail.com>
2020-06-14 14:40:32 -0600

    added trailing slash for repo URL

--- cookbooks/first_cookbook/recipes/powershell_repos.rb
+++ cookbooks/first_cookbook/recipes/powershell_repos.rb
@@ -1,5 +1,5 @@
 powershell_package_source 'MyGetPowerShell' do
-  url           'https://healthwise.myget.org/F/powershellmodules/api/v2'
+  url           'https://healthwise.myget.org/F/powershellmodules/api/v2/'
   provider_name 'NuGet'
   trusted       true
   action        :register
