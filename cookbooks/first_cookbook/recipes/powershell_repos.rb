a6556f16f3f93be5d4e46b6320ecf71b4649a3bb
Luke Alton <lucas.alton@gmail.com>
2020-06-14 15:23:49 -0600

    updated provider type for PS repo recipe

--- cookbooks/first_cookbook/recipes/powershell_repos.rb
+++ cookbooks/first_cookbook/recipes/powershell_repos.rb
@@ -1,6 +1,6 @@
 powershell_package_source 'MyGetPowerShell' do
   url           'https://healthwise.myget.org/F/powershellmodules/api/v2/'
-  provider_name 'NuGet'
+  provider_name 'PowerShellGet'
   trusted       true
   action        :register
 end
