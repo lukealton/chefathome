847115f023ca5af2bb888f724f32bece5f0b48c8
Luke Alton <lucas.alton@gmail.com>
2020-06-19 11:20:56 -0600

    added version to ssms recipe

--- cookbooks/first_cookbook/recipes/ssms_18.rb
+++ cookbooks/first_cookbook/recipes/ssms_18.rb
@@ -2,6 +2,7 @@ windows_package 'SQL Server Management Studio' do
   source         'C:\Users\Luke\Downloads\SSMS-Setup-ENU.exe'
   installer_type :custom
   checksum       'ABC37A45E16C099D7D4CFBE9E16C238C007BCB20FF1FE6BF0FFE916AA2084BEA'
+  version        '18.5.1'
   options        '/Quiet'
   action         :install
 end
