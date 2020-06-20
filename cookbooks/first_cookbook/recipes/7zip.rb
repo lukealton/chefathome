a4f8e1d7a69c6f5b63fbecac9ab2aab24aaacd02
Luke Alton <lucas.alton@gmail.com>
2020-06-20 10:24:44 -0600

    reorganizing recipes

--- cookbooks/first_cookbook/recipes/7zip.rb
+++ cookbooks/first_cookbook/recipes/7zip.rb
@@ -1,6 +1,6 @@
 windows_package '7-Zip 19.00 (x64 edition)' do
-    checksum       'A7803233EEDB6A4B59B3024CCF9292A6FFFB94507DC998AA67C5B745D197A5DC'
-    source         'https://www.7-zip.org/a/7z1900-x64.msi'
-    installer_type :msi
-    action         :install
+  checksum       'A7803233EEDB6A4B59B3024CCF9292A6FFFB94507DC998AA67C5B745D197A5DC'
+  source         'https://www.7-zip.org/a/7z1900-x64.msi'
+  installer_type :msi
+  action         :install
 end
--- cookbooks/first_cookbook/recipes/default.rb
+++ cookbooks/first_cookbook/recipes/default.rb
@@ -8,12 +8,6 @@ file "#{ENV['HOME']}/test.txt" do
   content 'Rainbow sparkle land achieved :) !'
 end
 
-windows_package '7-Zip 19.00 (x64 edition)' do
-  checksum       'A7803233EEDB6A4B59B3024CCF9292A6FFFB94507DC998AA67C5B745D197A5DC'
-  source         'https://www.7-zip.org/a/7z1900-x64.msi'
-  installer_type :msi
-  action         :install
-end
-
 include_recipe 'first_cookbook::powershell_core'
-include_recipe 'first_cookbook::powershell_repos'
+include_recipe 'first_cookbook::7zip'
+include_recipe 'first_cookbook::ssms_18'
