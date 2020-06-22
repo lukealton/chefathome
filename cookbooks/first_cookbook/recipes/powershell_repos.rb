870c2857132e476f3bbc7404ff8a4d2850feb239
Luke Alton <lucas.alton@gmail.com>
2020-06-21 23:45:04 -0600

    added back powershell_repos recipe

--- cookbooks/first_cookbook/recipes/powershell_repos.rb
+++ cookbooks/first_cookbook/recipes/powershell_repos.rb
@@ -1,6 +1,13 @@
-powershell_package_source 'MyGetPowerShell' do
-  url           'https://healthwise.myget.org/F/powershellmodules/api/v2/'
-  provider_name 'PowerShellGet'
-  trusted       true
-  action        :register
+dbag = data_bag_item('secrets', 'mygetapikey')
+password = dbag["password"]
+username = dbag["username"]
+
+powershell_script 'Register HW MyGet PS Repo' do
+  code <<-EOH
+  $User = #{username}
+  $PWord = ConvertTo-SecureString -String #{password} -AsPlainText -Force
+  $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+  Register-PSRepository -Name MyGetPowerShell –SourceLocation https://healthwise.myget.org/F/powershellmodules/api/v2 -InstallationPolicy Trusted -Credential $credential
+  EOH
+  not_if '(Get-PSRepository).SourceLocation -contains 'https://healthwise.myget.org/F/powershellmodules/api/v2''
 end
